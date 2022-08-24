b3e07a52f1595a3fdd943370474fc9ff47856a8b
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-24 23:57:36 +0430

    Initial Repo

---
+++ SortingLinkedList_Ascending/SortingLinkedList_Ascending/Program.cs
@@ -0,0 +1,15 @@
+﻿using SortingLinkedList_Ascending;
+
+var list = new LinkedList();
+
+list.Insert(44);
+list.Insert(2000);
+list.Insert(550);
+list.Insert(3);
+list.Insert(1);
+
+list.SortByAscending();
+
+list.Print();
+
+Console.ReadKey();
